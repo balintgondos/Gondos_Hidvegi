b55e6b9b748787d08823bfcf66960aff1ac4ca47
Peter <peter.hidvegi92@gmail.com>
2015-04-15 01:15:14 +0200

    Ébresztés ikon és idő kijelzés beállítás kész

--- app/src/main/java/hu/uniobuda/nik/gondos_hidvegi/MainActivity.java
+++ app/src/main/java/hu/uniobuda/nik/gondos_hidvegi/MainActivity.java
@@ -6,7 +6,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -15,6 +18,24 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Ebresztes> ebresztesek = new ArrayList<Ebresztes>();
+        ebresztesek.add(new Ebresztes(false,"9:00","Ebresztő",3));
+        ebresztesek.add(new Ebresztes(true,"9:10","Ebresztő",4));
+        ebresztesek.add(new Ebresztes(false,"9:00","Ebresztő",3));
+        ebresztesek.add(new Ebresztes(true,"9:10","Ebresztő",4));
+        ebresztesek.add(new Ebresztes(false,"9:00","Ebresztő",3));
+        ebresztesek.add(new Ebresztes(true,"9:10","Ebresztő",4));
+        ebresztesek.add(new Ebresztes(false,"9:00","Ebresztő",3));
+        ebresztesek.add(new Ebresztes(true,"9:10","Ebresztő",4));
+        ebresztesek.add(new Ebresztes(false,"9:00","Ebresztő",3));
+        ebresztesek.add(new Ebresztes(true,"9:10","Ebresztő",4));
+        ebresztesek.add(new Ebresztes(false,"9:00","Ebresztő",3));
+        ebresztesek.add(new Ebresztes(true,"9:10","Ebresztő",4));
+
+        EbresztesAdapter ebresztesAdapter = new EbresztesAdapter(ebresztesek);
+        ListView lv = (ListView) findViewById(R.id.idListView);
+        lv.setAdapter(ebresztesAdapter);
 
     }
 
